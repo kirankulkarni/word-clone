d2ce6b8a6807883fe5921449d46e8505a19ef8a3
Kiran Kulkarni <kk.questworld@gmail.com>
2023-03-29 17:18:45 +0530

    Handle  word in Guess rendering

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 function Guess({ guessWord }) {
+  const word = guessWord || '';
   return (
     <>
-      <span className='cell'>{guessWord[0] || ''}</span>
-      <span className='cell'>{guessWord[1] || ''}</span>
-      <span className='cell'>{guessWord[2] || ''}</span>
-      <span className='cell'>{guessWord[3] || ''}</span>
-      <span className='cell'>{guessWord[4] || ''}</span>
+      <span className='cell'>{word[0] || ''}</span>
+      <span className='cell'>{word[1] || ''}</span>
+      <span className='cell'>{word[2] || ''}</span>
+      <span className='cell'>{word[3] || ''}</span>
+      <span className='cell'>{word[4] || ''}</span>
     </>
   );
 }
